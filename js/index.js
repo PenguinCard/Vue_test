63d3dd00b8173c47af6c4bc4e4efa1a30255bd3e
PenguinCard <ymist17@gmail.com>
2021-03-13 01:17:17 +0900

    add practice

--- js/index.js
+++ js/index.js
@@ -5,4 +5,48 @@ window.onload = ()=>{
             msg : 'Hello Vue~!'
         }
     })
+
+    let app2 = new Vue({
+        el : '#app-2',
+        data : {
+            msg : 'Page? '+new Date() +' load'
+        }
+    })
+
+    let app3 = new Vue({
+        el : '#app-3',
+        data : {
+            seen : true
+        }
+    })
+
+    let app4 = new Vue({
+        el : '#app-4',
+        data : {
+            todos : [
+                { text : 'test1'},
+                { text : 'test2'},
+                { text : 'test3'}
+            ]
+        }
+    })
+
+    new Vue({
+        el : '#app-5',
+        data : {
+            msg : 'Hello!Vue!'
+        }, 
+        methods : {
+            reverseMsg : function(){
+                this.msg = this.msg.split('').reverse().join('')
+            }
+        }
+    })
+
+    new Vue({
+        el : '#app-6',
+        data : {
+            msg : 'Hello Vue~!'
+        }
+    })
 }
