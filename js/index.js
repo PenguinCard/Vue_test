e2c5c6f7f2327cd93b6044222ca16960fec9377d
PenguinCard <ymist17@gmail.com>
2021-03-14 22:26:29 +0900

    form check test

--- js/index.js
+++ js/index.js
@@ -1,52 +1,10 @@
 window.onload = ()=>{
-    let app = new Vue({
-        el : '#app',
-        data : {
-            msg : 'Hello Vue~!'
-        }
-    })
-
-    let app2 = new Vue({
-        el : '#app-2',
-        data : {
-            msg : 'Page? '+new Date() +' load'
-        }
-    })
-
-    let app3 = new Vue({
-        el : '#app-3',
-        data : {
-            seen : true
-        }
-    })
-
-    let app4 = new Vue({
-        el : '#app-4',
-        data : {
-            todos : [
-                { text : 'test1'},
-                { text : 'test2'},
-                { text : 'test3'}
-            ]
-        }
-    })
-
-    new Vue({
-        el : '#app-5',
-        data : {
-            msg : 'Hello!Vue!'
-        }, 
-        methods : {
-            reverseMsg : function(){
-                this.msg = this.msg.split('').reverse().join('')
-            }
-        }
-    })
-
-    new Vue({
-        el : '#app-6',
-        data : {
-            msg : 'Hello Vue~!'
-        }
-    })
+     new Vue({
+         el : '#app',
+         data : {
+             home : false,
+             a : './img/home.png',
+             b : './img/tree.png'
+         }
+     })
 }
